160d9e41a7b344c63fb9450a4be7bf7e7cdf25fa
Jonathan Wilson <jon.wils15@gmail.com>
2021-03-20 13:11:59 +0000

    add item feature added. Step skeletons added also

---
+++ features/add_to_basket.feature
@@ -0,0 +1,16 @@
+Feature: As a user, I want to add an item I like, so that I can purchase it later on
+
+    Scenario: Add initial item
+        Given the user's basket has 0 items in it
+        When the user adds the item to the basket
+        Then the user's basket has 1 items in it
+
+    Scenario: Add more items
+        Given the user's basket has at least 1 item
+        When the user adds the item to the basket
+        Then the user's basket has one more item in it
+
+    Scenario: Add item multiple times
+        Given the user's basket has at least 1 item
+        When the user adds the item to the basket
+        Then the user's basket has one more item in it
---
+++ features/steps/add_to_basket_steps.py
@@ -0,0 +1,35 @@
+from behave import given, when, then
+
+@given(u'the user\'s basket has 0 items in it')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the user\'s basket has 0 items in it')
+
+
+@when(u'the user adds the item to the basket')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the user adds the item to the basket')
+
+
+@then(u'the user\'s basket has 1 items in it')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the user\'s basket has 1 items in it')
+
+
+@given(u'the user\'s basket has at least 1 item')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the user\'s basket has at least 1 item')
+
+
+@then(u'the user\'s basket has one more item in it')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the user\'s basket has one more item in it')
+
+
+@given(u'the user\'s basket has at least 1 item')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the user\'s basket has at least 1 item')
+
+
+@then(u'the user\'s basket has one more item in it')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the user\'s basket has one more item in it')
